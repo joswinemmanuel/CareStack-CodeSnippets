cc49292f7cbd42019d8fde9c053082d5b94e9f75
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-08-08 14:26:54 +0530

    The Designer will be empty but perfect report during preview

---
+++ EmptyDesignerButPreview.cs
@@ -0,0 +1,262 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using Telerik.Reporting;
+using Telerik.Reporting.Drawing;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using TextBox = Telerik.Reporting.TextBox;
+
+namespace TelerikReportWebAPI.Reports
+{
+    /// <summary>
+    /// Summary description for Report2.
+    /// </summary>
+    public partial class Report2 : Telerik.Reporting.Report
+    {
+        public Report2()
+        {
+            //
+            // Required for telerik Reporting designer support
+            //
+            InitializeComponent();
+            var data = GetData();
+            this.table1.DataSource = data;
+            AddDynamicColumns(data);
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+
+        private void InitializeComponent()
+        {
+            Telerik.Reporting.TableGroup tableGroup1 = new Telerik.Reporting.TableGroup();
+            Telerik.Reporting.TableGroup tableGroup2 = new Telerik.Reporting.TableGroup();
+            Telerik.Reporting.Drawing.StyleRule styleRule1 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.StyleRule styleRule2 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.StyleRule styleRule3 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.DescendantSelector descendantSelector1 = new Telerik.Reporting.Drawing.DescendantSelector();
+            Telerik.Reporting.Drawing.StyleRule styleRule4 = new Telerik.Reporting.Drawing.StyleRule();
+            Telerik.Reporting.Drawing.DescendantSelector descendantSelector2 = new Telerik.Reporting.Drawing.DescendantSelector();
+            this.textBox1 = new Telerik.Reporting.TextBox();
+            this.detailSection1 = new Telerik.Reporting.DetailSection();
+            this.table1 = new Telerik.Reporting.Table();
+            this.textBox2 = new Telerik.Reporting.TextBox();
+            this.pageHeaderSection1 = new Telerik.Reporting.PageHeaderSection();
+            this.pageFooterSection1 = new Telerik.Reporting.PageFooterSection();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            this.textBox1.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(0D);
+            this.textBox1.Style.Visible = false;
+            this.textBox1.StyleName = "Normal.TableHeader";
+            this.textBox1.Value = "";
+            // 
+            // detailSection1
+            // 
+            this.detailSection1.Height = Telerik.Reporting.Drawing.Unit.Cm(2.5D);
+            this.detailSection1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.table1});
+            this.detailSection1.Name = "detailSection1";
+            // 
+            // table1
+            // 
+            this.table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(2D)));
+            this.table1.Body.Rows.Add(new Telerik.Reporting.TableBodyRow(Telerik.Reporting.Drawing.Unit.Cm(0.5D)));
+            this.table1.Body.SetCellContent(0, 0, this.textBox2);
+            tableGroup1.Name = "countryId";
+            tableGroup1.ReportItem = this.textBox1;
+            this.table1.ColumnGroups.Add(tableGroup1);
+            this.table1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.textBox2,
+            this.textBox1});
+            this.table1.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(0D), Telerik.Reporting.Drawing.Unit.Cm(0D));
+            this.table1.Name = "table1";
+            tableGroup2.Groupings.Add(new Telerik.Reporting.Grouping(null));
+            tableGroup2.Name = "detail";
+            this.table1.RowGroups.Add(tableGroup2);
+            this.table1.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2D), Telerik.Reporting.Drawing.Unit.Cm(1D));
+            this.table1.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(0D);
+            this.table1.StyleName = "Normal.TableNormal";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            this.textBox2.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(0D);
+            this.textBox2.Style.Visible = false;
+            this.textBox2.StyleName = "Normal.TableBody";
+            this.textBox2.Value = "";
+            // 
+            // pageHeaderSection1
+            // 
+            this.pageHeaderSection1.Height = Telerik.Reporting.Drawing.Unit.Cm(2.5D);
+            this.pageHeaderSection1.Name = "pageHeaderSection1";
+            // 
+            // pageFooterSection1
+            // 
+            this.pageFooterSection1.Height = Telerik.Reporting.Drawing.Unit.Cm(2.5D);
+            this.pageFooterSection1.Name = "pageFooterSection1";
+            // 
+            // Report2
+            // 
+            this.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            this.detailSection1,
+            this.pageHeaderSection1,
+            this.pageFooterSection1});
+            this.Name = "Report2";
+            this.PageSettings.Margins = new Telerik.Reporting.Drawing.MarginsU(Telerik.Reporting.Drawing.Unit.Mm(20D), Telerik.Reporting.Drawing.Unit.Mm(20D), Telerik.Reporting.Drawing.Unit.Mm(20D), Telerik.Reporting.Drawing.Unit.Mm(20D));
+            this.PageSettings.PaperKind = System.Drawing.Printing.PaperKind.A4;
+            styleRule1.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.TextItemBase)),
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.HtmlTextBox))});
+            styleRule1.Style.Padding.Left = Telerik.Reporting.Drawing.Unit.Point(2D);
+            styleRule1.Style.Padding.Right = Telerik.Reporting.Drawing.Unit.Point(2D);
+            styleRule2.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.StyleSelector(typeof(Telerik.Reporting.Table), "Normal.TableNormal")});
+            styleRule2.Style.BorderColor.Default = System.Drawing.Color.Black;
+            styleRule2.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
+            styleRule2.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(1D);
+            styleRule2.Style.Color = System.Drawing.Color.Black;
+            styleRule2.Style.Font.Name = "Tahoma";
+            styleRule2.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(9D);
+            descendantSelector1.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.Table)),
+            new Telerik.Reporting.Drawing.StyleSelector(typeof(Telerik.Reporting.ReportItem), "Normal.TableBody")});
+            styleRule3.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            descendantSelector1});
+            styleRule3.Style.BorderColor.Default = System.Drawing.Color.Black;
+            styleRule3.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
+            styleRule3.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(1D);
+            styleRule3.Style.Font.Name = "Tahoma";
+            styleRule3.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(9D);
+            descendantSelector2.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            new Telerik.Reporting.Drawing.TypeSelector(typeof(Telerik.Reporting.Table)),
+            new Telerik.Reporting.Drawing.StyleSelector(typeof(Telerik.Reporting.ReportItem), "Normal.TableHeader")});
+            styleRule4.Selectors.AddRange(new Telerik.Reporting.Drawing.ISelector[] {
+            descendantSelector2});
+            styleRule4.Style.BorderColor.Default = System.Drawing.Color.Black;
+            styleRule4.Style.BorderStyle.Default = Telerik.Reporting.Drawing.BorderType.Solid;
+            styleRule4.Style.BorderWidth.Default = Telerik.Reporting.Drawing.Unit.Pixel(1D);
+            styleRule4.Style.Font.Name = "Tahoma";
+            styleRule4.Style.Font.Size = Telerik.Reporting.Drawing.Unit.Point(10D);
+            styleRule4.Style.VerticalAlign = Telerik.Reporting.Drawing.VerticalAlign.Middle;
+            this.StyleSheet.AddRange(new Telerik.Reporting.Drawing.StyleRule[] {
+            styleRule1,
+            styleRule2,
+            styleRule3,
+            styleRule4});
+            this.Width = Telerik.Reporting.Drawing.Unit.Cm(17D);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        private void AddDynamicColumns(DataTable dataTable)
+        {
+            int existingColumns = this.table1.Body.Columns.Count;
+
+            for (int i = existingColumns; i < dataTable.Columns.Count; i++)
+            {
+                string columnName = dataTable.Columns[i-1].ColumnName;
+
+                this.table1.Body.Columns.Add(new TableBodyColumn(Unit.Cm(2)));
+
+                var headerTextBox = new TextBox
+                {
+                    Name = $"textBox{i + 1}",
+                    Size = new SizeU(Unit.Cm(2), Unit.Cm(0.5)),
+                    StyleName = "Normal.TableHeader",
+                    Value = columnName
+                };
+
+                var tableGroup = new TableGroup();
+                tableGroup.ReportItem = headerTextBox;
+                this.table1.ColumnGroups.Add(tableGroup);
+
+                var bodyTextBox = new TextBox
+                {
+                    Name = $"textBox{i + existingColumns + 1}",
+                    Size = new SizeU(Unit.Cm(2), Unit.Cm(0.5)),
+                    StyleName = "Normal.TableBody",
+                    Value = $"= Fields.{columnName}"
+                };
+                this.table1.Body.SetCellContent(0, i, bodyTextBox);
+            }
+
+            this.table1.Size = new SizeU(Unit.Cm(2 * dataTable.Columns.Count+1), this.table1.Size.Height);
+        }
+        private DataTable GetData()
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=countrydb;Integrated Security=True";
+            string query = "SELECT CountryId, CountryName, Population, Area FROM Countries";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dataTable);
+
+                    var random = new Random();
+
+                    var columnsToAdd = new List<(string Name, Type Type, Func<DataRow, int, object> ValueGenerator)>
+                    {
+                        ("Password", typeof(string), (DataRow row, int index) => GenerateRandomString(8, random)),
+                        ("Gratitude", typeof(string), (DataRow row, int index) => $"Thank God {index + 1}"),
+                        ("Number", typeof(int), (DataRow row, int index) => random.Next(1, 101)),
+                        ("Person", typeof(string), (DataRow row, int index) => $"Person {index+1}"),
+                    };
+
+                    AddColumnsToDataTable(dataTable, columnsToAdd);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+                return dataTable;
+            }
+        }
+
+        private void AddColumnsToDataTable(DataTable dataTable, List<(string Name, Type Type, Func<DataRow, int, object> ValueGenerator)> columnsToAdd)
+        {
+            foreach (var (name, type, valueGenerator) in columnsToAdd)
+            {
+                dataTable.Columns.Add(name, type);
+            }
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                DataRow row = dataTable.Rows[i];
+                foreach (var (name, _, valueGenerator) in columnsToAdd)
+                {
+                    row[name] = valueGenerator(row, i);
+                }
+            }
+        }
+
+        private static string GenerateRandomString(int length, Random random)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private Telerik.Reporting.DetailSection detailSection1;
+        private Telerik.Reporting.Table table1;
+        private Telerik.Reporting.TextBox textBox2;
+        private Telerik.Reporting.TextBox textBox1;
+        private Telerik.Reporting.PageHeaderSection pageHeaderSection1;
+        private Telerik.Reporting.PageFooterSection pageFooterSection1;
+    }
+}
